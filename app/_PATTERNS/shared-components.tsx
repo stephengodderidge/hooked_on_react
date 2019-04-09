8a33226dbe323cb162a1c2ae1d17b17be2ec3ea2
Stephen Godderidge <stephen.godderidge@parivedasolutions.com>
2019-04-09 15:34:34 -0400

    removed app/_Docs/ and /_Patterns/

--- app/_PATTERNS/shared-components.tsx
+++
@@ -1,9 +0,0 @@
-import React, { SFC } from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  padding: 16px;
-`;
-
-/** Wrapper component for examples */
-export const ExampleWrapper: SFC<{}> = props => <Wrapper>{props.children}</Wrapper>;
